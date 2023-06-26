5818232dd57494def7cf970eac1150bbfdcaf6ef
Nesza733 <107313010+Nesza733@users.noreply.github.com>
2023-06-26 15:24:25 +0700

    add message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Header';
+import Footer from './Footer';
+import { useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const [num, setNum] = useState(0);
+    let isPrime = '';
+    if (num === 1){
+      isPrime = (`${num} เป็นทั้งจำนวนเฉพาะและ<strong>ไม่</strong>เป็นจำนวนเฉพาะ`)
+    }
+    else if (num > 1){
+      for (let i = 2; i < num; i++){
+        if(num % i === 0){
+          isPrime = (`${num} เป็นจำนวนเฉพาะ`);
+        }
+      }
+      if (isPrime){
+        isPrime = (`${num}<strong> ไม่</strong>เป็นจำนวนเฉพาะ`)
+      }else{
+        isPrime = (`${num} เป็นจำนวนเฉพาะ`);
+      }
+    }
+  return(
+    <div className='App'>
+      <Header />
+      <div className='MyContent'>
+        <h2>{num}</h2>
+        <button onClick={()=>setNum(num + 1)} class="btn btn-primary">เพิ่ม</button>
+        &nbsp;
+        <button onClick={()=>setNum(num - 1)} class="btn btn-secondary">ลด</button>
+        <br></br>
+        <span dangerouslySetInnerHTML={{__html: isPrime}}></span> 
+      </div>
+      <Footer />
     </div>
   );
 }
